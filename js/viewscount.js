4fa00a160377fe272891f67a5cb7fa44941563f1
MasCreepy <61155037+MasCreepy@users.noreply.github.com>
2021-07-16 08:18:54 +0700

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -1,9 +1,9 @@
-const countEl = document.getElementById('count');
+const countEl = document.getElementByClassName('count')[0];
 
 updateVisitCount();
 
 function updateVisitCount() {
-  fetch('')
+  fetch('https://api.countapi.xyz/update/kripik/kripek/?amount=1')
   .then(res => res.json())
   .then(res => {
     countEl.innerHTML = res.value;
